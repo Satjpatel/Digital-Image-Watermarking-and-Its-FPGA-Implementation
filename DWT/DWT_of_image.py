db1abd85b73e794a7a576bafe3b4c4d4af42d35d
Sat Patel <satjpatel007@gmail.com>
2021-09-13 21:57:16 +0530

    Image construction and reconstruction done

--- DWT/DWT_of_image.py
+++ DWT/DWT_of_image.py
@@ -11,9 +11,13 @@ import cv2 as cv
 import pywt
 import numpy as np
 import matplotlib.pyplot as plotty
+
+''' 
+
+Level 1 - DWT and IDWT 
 # Get the image
 
-img = cv.imread('Lenna.png',0) 
+img = cv.imread('springer.png',0) 
 
 cv.imshow('Kung Fu Panda Original',img) 
 cv.waitKey(9000)
@@ -36,4 +40,45 @@ cv.waitKey(0)
 img_r = pywt.idwt2(coff, 'db3', mode = 'symmetric') 
 img_r_dash = img_r.astype(np.uint8)
 cv.imshow('Reconstructed Image', img_r_dash)
+cv.waitKey(0)
+
+''' 
+
+# Now working on 2 Level Discrete Wavelet Transforms 
+
+img = cv.imread('Lenna.png',0) 
+
+cv.imshow('Lenna Original',img) 
+cv.waitKey(0)
+
+coeff_level2 = pywt.wavedec2(img, 'db3', mode = 'symmetric', level=2 )
+[cA2, (cH2, cV2, cD2), (cH1, cV1, cD1)] = coeff_level2 
+
+# Level 1 Decomposition Images 
+img_intermediate = pywt.idwt2((cA2, (cH2, cV2, cD2)), 'db3',mode = 'symmetric') 
+plotty.subplot(2,2,1)
+cv.imshow('Approximate Details', img_intermediate.astype(np.uint8))
+plotty.subplot(2,2,2)
+cv.imshow('Vertical Details (cV)', cV1.astype(np.uint8))
+plotty.subplot(2,2,3)
+cv.imshow('Horizontal Details (cH)', cH1.astype(np.uint8))
+plotty.subplot(2,2,4)
+cv.imshow('Diagonal Details (cD)', cD1.astype(np.uint8))
+cv.waitKey(0)
+
+
+# Level 2 Decomposition 
+plotty.subplot(2,2,1)
+cv.imshow('Approximate Details', cA2.astype(np.uint8))
+plotty.subplot(2,2,2)
+cv.imshow('Vertical Details (cV)', cV2.astype(np.uint8))
+plotty.subplot(2,2,3)
+cv.imshow('Horizontal Details (cH)', cH2.astype(np.uint8))
+plotty.subplot(2,2,4)
+cv.imshow('Diagonal Details (cD)', cD2.astype(np.uint8))
+cv.waitKey(0)
+
+# Reconstructing the image using Inverse DWT 
+img_reconstructed = pywt.waverec2(coeff_level2, 'db3',mode = 'symmetric') 
+cv.imshow('Reconstructed Lenna Image', img_reconstructed.astype(np.uint8))
 cv.waitKey(0)
